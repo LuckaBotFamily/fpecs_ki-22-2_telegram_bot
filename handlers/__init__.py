7961103499cf98e418cec2a8b11e9c957490ce65
Maxim <luckyakalucka.devel@gmail.com>
2022-12-08 19:39:01 +0200

    Update

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,3 +2,4 @@ from handlers import days
 from handlers import notify
 from handlers import timetable
 from handlers import commands
+from handlers import days_mess
---
+++ handlers/days_mess.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+import gspread
+worksheet = gspread.service_account(filename='level-slate-280111-4930953f5702.json').open_by_url('https://docs.google.com/spreadsheets/d/1ZLodGxsGLLxDdzVbpING0jcJImsyOAW1S9y8NcAs2_w/edit#gid=2127480627').get_worksheet(0)
+
+
+def day_mess(day):
+    if int(datetime.date(datetime.today()).strftime("%V")) % 2 == 0:
+        if str(day) == "monday":
+            text = "    ◀ Понеділок ▶ 🔶  \n"
+            x = 4
+        if str(day) == "tuesday":
+            text = "    ◀ Вівторок ▶ 🔶  \n"
+            x = 14
+        if str(day) == "wednesday":
+            text = "    ◀ Середа ▶ 🔶  \n"
+            x = 24
+        if str(day) == "thursday":
+            text = "    ◀ Четверг ▶ 🔶  \n"
+            x = 34
+        if str(day) == "friday":
+            text = "    ◀ П'ятниця ▶ 🔶  \n"
+            x = 44
+    else:
+        if str(day) == "monday":
+            text = "    ◀ Понеділок ▶ 🔷  \n"
+            x = 3
+        if str(day) == "tuesday":
+            text = "    ◀ Вівторок ▶ 🔷  \n"
+            x = 13
+        if str(day) == "wednesday":
+            text = "    ◀ Середа ▶ 🔷  \n"
+            x = 23
+        if str(day) == "thursday":
+            text = "    ◀ Четверг ▶ 🔷  \n"
+            x = 33
+        if str(day) == "friday":
+            text = "    ◀ П'ятниця ▶ 🔷 \n"
+            x = 43
+        first = str(worksheet.acell('C' + str(x) + ':F' + str(x) + '').value)
+        second = str(worksheet.acell('C' + str(x + 2) + ':F' + str(x + 2) + '').value)
+        third = str(worksheet.acell('C' + str(x + 4) + ':F' + str(x + 4) + '').value)
+        fourth = str(worksheet.acell('C' + str(x + 6) + ':F' + str(x + 6) + '').value)
+        text += "════════════════\n"
+        if first != "None":
+            text += "Ⅰ.   [08.00 - 09.20] <b>" + first + "</b>\n"
+            pass
+        if second != "None":
+            text += "Ⅱ.  [09.35 - 10.55] <b>" + second + "</b>\n"
+            pass
+        if third != "None":
+            text += "Ⅲ. [11.10 - 12.30] <b>" + third + "</b>\n"
+            pass
+        if fourth != "None":
+            text += "Ⅳ. [12.45 - 14.05] <b>" + fourth + "</b>\n"
+            pass
+    return  text
