41373391bf07d6bd8dd4dbf17e23fe734f813f7e
Maxim <luckyakalucka.devel@gmail.com>
2022-12-23 17:08:14 +0200

    notify.py: fix & add googlesheets as bd

--- handlers/notify.py
+++ handlers/notify.py
@@ -1,154 +1,72 @@
 from datetime import datetime
+
+import gspread
 from aiogram import types, Dispatcher
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from .days import monday, tuesday, wednesday, thursday, friday
+from . import days_mess, days
 from create_bot import bot, dp
 
-notifyFile = open("notify.txt", "r")
-notifyUsers = set ()
-for line in notifyFile:
-    notifyUsers.add(line.strip())
-notifyFile.close()
+worksheet = gspread.service_account(filename='level-slate-280111-4930953f5702.json').open_by_url('https://docs.google.com/spreadsheets/d/1ZLodGxsGLLxDdzVbpING0jcJImsyOAW1S9y8NcAs2_w/edit#gid=2127480627').get_worksheet(2)
+
+async def notify_togl(message: types.Message):
+    if message.get_args() == "on":
+        x = 1
+        if str(message.chat.id) in worksheet.col_values(2):
+                text = 'Повідомлення вже увімкнені'
+        else:
+            while (x < 100):
+                if str(worksheet.acell(f"B{x}").value) == "None":
+                    worksheet.update(f"B{x}", str(message.chat.id))
+                    if message.chat.id == message.from_user.id:
+                        worksheet.update(f"A{x}", str(message.from_user.username))
+                    else:
+                        worksheet.update(f"A{x}", str("Chat"))
+                    text = 'Повідомлення успішно увімкнені'
+                    break
+                x = x + 1
+    elif message.get_args() == "off":
+        worksheet.update(f"A{worksheet.find(str(message.chat.id)).row}", "None")
+        worksheet.update(f"B{worksheet.find(str(message.chat.id)).row}", "None")
+        text = 'Повідомлення успішно вимкнено'
+    else:
+        text = 'Будь ласка, введіть аргумент "on" щоб увімкнути повідомлення, або "off" щоб їх вимкнути'
+    await bot.send_message(chat_id=message.chat.id, text=text)
 
 async def notify(pars):
-    global newday_mess
     week = datetime.date(datetime.today()).strftime("%V")
-    for user in notifyUsers:
+    for user in worksheet.col_values(2):
         if int(week) % 2 == 0:
-            if datetime.weekday(datetime.today()) == 0:
-                if int(pars) == 3:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.mon_b_3}')
-                if int(pars) == 4:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.mon_4}')
-            if datetime.weekday(datetime.today()) == 1:
-                if int(pars) == 1:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.tue_b_1}')
-                if int(pars) == 2:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.tue_2}')
-                if int(pars) == 3:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.tue_3}')
-            if datetime.weekday(datetime.today()) == 2:
-                if int(pars) == 1:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.wed_1}')
-                if int(pars) == 3:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.wed_b_3}')
-                if int(pars) == 4:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.wed_b_4}')
-            if datetime.weekday(datetime.today()) == 3:
-                if int(pars) == 2:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.thu_b_2}')
-                if int(pars) == 3:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.thu_b_3}')
-                if int(pars) == 4:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.thu_4}')
-            if datetime.weekday(datetime.today()) == 4:
-                if int(pars) == 1:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.fri_1}')
-                if int(pars) == 2:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.fri_2}')
-                if int(pars) == 3:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.fri_3}')
-                if int(pars) == 4:
-                    pass
+            line = str(days_mess.getLine(day=datetime.isoweekday(datetime.today()), color=0, line=pars))
         else:
-            if datetime.weekday(datetime.today()) == 0:
-                if int(pars) == 3:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.mon_a_3}')
-                if int(pars) == 4:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.mon_4}')
-            if datetime.weekday(datetime.today()) == 1:
-                if int(pars) == 1:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.tue_a_1}')
-                if int(pars) == 2:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.tue_2}')
-                if int(pars) == 3:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.tue_3}')
-            if datetime.weekday(datetime.today()) == 2:
-                if int(pars) == 1:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.wed_1}')
-                if int(pars) == 2:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.wed_a_2}')
-                if int(pars) == 3:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.wed_a_3}')
-                if int(pars) == 4:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.wed_a_4}')
-                pass
-            if datetime.weekday(datetime.today()) == 3:
-                if int(pars) == 2:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.thu_a_2}')
-                if int(pars) == 3:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.thu_a_3}')
-                if int(pars) == 4:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.thu_4}')
-            if datetime.weekday(datetime.today()) == 4:
-                if int(pars) == 1:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.fri_1}')
-                if int(pars) == 2:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.fri_2}')
-                if int(pars) == 3:
-                    await bot.send_message(chat_id=user,
-                                           text=f'Через 5 минут начнется пара {config.fri_3}')
-
-async def notify_on(message: types.Message):
-    notifyFile = open("notify.txt", "r")
-    if not str(message.chat.id) in notifyFile:
-        notifyFile.close()
-        notifyFile = open("notify.txt", "a")
-        notifyFile.write(str(message.chat.id) + "\n")
-        notifyUsers.add(message.chat.id)
-        await bot.send_message(message.chat.id, 'Уведомления включены')
-
+            line = str(days_mess.getLine(day=datetime.isoweekday(datetime.today()), color=1, line=pars))
+        message = await bot.send_message(chat_id=user, text=f"Через 5 хвилин розпочнеться пара\n<b>{line}</b>")
+        await bot.pin_chat_message(chat_id=user, message_id=message.message_id)
 
 async def newday():
     global newday_mess
     if datetime.weekday(datetime.today()) == 0:
-        newday_mess = await monday(chat_id=-1001709052184)
+        text = days_mess.day_mess("monday")
     if datetime.weekday(datetime.today()) == 1:
-        newday_mess = await tuesday(chat_id=-1001709052184)
+        text = days_mess.day_mess("tuesday")
     if datetime.weekday(datetime.today()) == 2:
-        newday_mess = await wednesday(chat_id=-1001709052184)
+        text = days_mess.day_mess("wednesday")
     if datetime.weekday(datetime.today()) == 3:
-        newday_mess = await thursday(chat_id=-1001709052184)
+        text = days_mess.day_mess("thursday")
     if datetime.weekday(datetime.today()) == 4:
-        newday_mess = await friday(chat_id=-1001709052184)
-    await bot.pin_chat_message(chat_id=newday_mess.chat.id, message_id=newday_mess.message_id)
+        text = days_mess.day_mess("friday")
+    for user in worksheet.col_values(2):
+        message = await bot.send_message(chat_id=user, text=text)
+        await bot.pin_chat_message(chat_id=user, message_id=message.message_id)
 
 
 scheduler = AsyncIOScheduler()
-scheduler.add_job(newday, 'cron', day_of_week='mon-fri', hour='5', minute='00')
-scheduler.add_job(notify, 'cron', day_of_week='mon-fri', hour='04', minute='55', args=('1'))
-scheduler.add_job(notify, 'cron', day_of_week='mon-fri', hour='06', minute='30', args=('2'))
-scheduler.add_job(notify, 'cron', day_of_week='mon-fri', hour='08', minute='05', args=('3'))
-scheduler.add_job(notify, 'cron', day_of_week='mon-fri', hour='09', minute='40', args=('4'))
+scheduler.add_job(newday, 'cron', day_of_week='mon-fri', hour='7', minute='50')
+scheduler.add_job(notify, 'cron', day_of_week='mon-fri', hour='07', minute='55', args=1)
+scheduler.add_job(notify, 'cron', day_of_week='mon-fri', hour='09', minute='30', args=2)
+scheduler.add_job(notify, 'cron', day_of_week='mon-fri', hour='11', minute='05', args=3)
+scheduler.add_job(notify, 'cron', day_of_week='mon-fri', hour='12', minute='40', args=4)
 scheduler.start()
 
 def register_handlers_notify(dp: Dispatcher):
-    dp.register_message_handler(notify_on, commands=['notify_on'])
+    dp.register_message_handler(notify_togl, commands=['notify'])
+    dp.register_message_handler(notify, commands=['notify_test'])
