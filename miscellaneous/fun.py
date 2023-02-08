08898159413c91566bf8dfe7b05d696fee0a8079
Maxim <luckyakalucka.devel@gmail.com>
2023-02-08 23:11:43 +0200

    misc: fun.py: sex() & kuni() & blowjob()

--- miscellaneous/fun.py
+++ miscellaneous/fun.py
@@ -5,6 +5,8 @@ import logger, config
 from aiogram import types, Dispatcher
 # 29.01.23 TODO: Переделать на нормальную базу, а то щас ряльно кринж
 usrsSex = ['586715737', '456391767', '1211580868', '576092040', '714713631', '786290626', '750915341', '999694590', '467991052', '1414646966', '602838745']
+usrsKuni = ['1414646966', '1211580868']
+usrsBlowjob = ['586715737', '456391767', '576092040', '714713631', '786290626', '750915341', '999694590', '467991052', '602838745']
 async def sex(message: types.Message):
     try:
         text = f"Ви успішно зайнялися сексом з <a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.full_name}</a>"
@@ -13,9 +15,40 @@ async def sex(message: types.Message):
             usr = await bot.get_chat_member(chat_id=message.chat.id, user_id=usrsSex[random.randint(0, 10)])
             text = f"Ви успішно зайнялися сексом з <a href='tg://user?id={usr.user.id}'>{usr.user.full_name}</a>"
         else:
-            text = f"Ви успішно зайнялися сексом з{message.text[4:]}"
+            text = f"Ви успішно зайнялися сексом з {message.text[4:]}"
     await bot.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id, text=text)
     await logger.logger_mess(message)
+
+async def kuni(message: types.Message):
+    try:
+        if str(message.reply_to_message.from_user.id) in usrsKuni:
+            text = f"Ви успішно помочили носик об <a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.full_name}</a>"
+        else:
+            text = f"Куди ти зібравься пхати свого носа ?"
+    except:
+        if message.get_args().split(' ')[0] == '':
+            usr = await bot.get_chat_member(chat_id=message.chat.id, user_id=usrsKuni[random.randint(0, 1)])
+            text = f"Ви успішно помочили носик об <a href='tg://user?id={usr.user.id}'>{usr.user.full_name}</a>"
+        else:
+            text = f"Ви успішно помочили носик об {message.text[5:]}"
+    await bot.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id, text=text)
+    await logger.logger_mess(message)
+
+async def blowjob(message: types.Message):
+    try:
+        if str(message.reply_to_message.from_user.id) in usrsBlowjob:
+            text = f"Ви успішно обсмоктали хуй у <a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.full_name}</a>"
+        else:
+            text = f"Що ти хочешь обсмоктати ??"
+    except:
+        if message.get_args().split(' ')[0] == '':
+            usr = await bot.get_chat_member(chat_id=message.chat.id, user_id=usrsBlowjob[random.randint(0, 8)])
+            text = f"Ви успішно обсмоктали хуй у <a href='tg://user?id={usr.user.id}'>{usr.user.full_name}</a>"
+        else:
+            text = f"Ви успішно обсмоктали хуй у {message.text[9:]}"
+    await bot.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id, text=text)
+    await logger.logger_mess(message)
+
 
 async def getSexUser(message: types.Message):
     text = "Претенденти на випадковий секс:"
--- miscellaneous/main.py
+++ miscellaneous/main.py
@@ -6,5 +6,8 @@ def register_handlers_misc(dp: Dispatcher):
     dp.register_message_handler(textAndVoice.voiceAns, content_types=['voice'])
     dp.register_callback_query_handler(textAndVoice.acceptVoice, text=['acceptVoice_Ua', 'acceptVoice_Ru'])
     dp.register_message_handler(fun.sex, commands=['sex'])
+    dp.register_message_handler(fun.kuni, commands=['kuni'])
+    dp.register_message_handler(fun.blowjob, commands=['blowjob'])
+
     dp.register_message_handler(fun.getSexUser, commands=['sex_user'])
 #    dp.register_message_handler(fun.info, commands=['info'])
